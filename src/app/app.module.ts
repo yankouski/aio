78a09207318c16856c46cfaaff6bee72bd198242
Alexander Yankouski <ayankouski@cmgfi.com>
2018-08-30 22:56:59 -0400

    Config files changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoansComponent } from './simulator/loans/loans.component';
+
+const appRoutes: Routes = [
+  {path: '',  component: HomepageComponent},
+  {path: 'loans',  component: LoansComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent,
+    LoansComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
