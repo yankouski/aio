5db65fbfd34256a32d885cd89088ea483d621b27
Alexander Yankouski <ayankouski@cmgfi.com>
2018-08-30 22:56:33 -0400

    Loan and HomePage components added

---
+++ src/app/simulator/loans/loans.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core'
+
+@Component({
+    selector: 'sim-loans',
+    templateUrl: './loans.component.html',
+    styles: ['./loans.component.scss']
+})
+export class LoansComponent implements OnInit{
+    constructor() {}
+
+    ngOnInit(){
+
+    }
+}
